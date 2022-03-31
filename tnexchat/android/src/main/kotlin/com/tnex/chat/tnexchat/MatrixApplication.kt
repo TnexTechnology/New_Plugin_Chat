db1bd06c62cfac27e46a78e89306ca401ef195e9
Tnex <>
2022-03-31 15:32:52 +0700

    add library TnexMatrix

--- tnexchat/android/src/main/kotlin/com/tnex/chat/tnexchat/MatrixApplication.kt
+++ tnexchat/android/src/main/kotlin/com/tnex/chat/tnexchat/MatrixApplication.kt
@@ -26,7 +26,22 @@ class MatrixApplication : Application(), MatrixConfiguration.Provider {
         initAppData()
         super.onCreate()
 
-        tnexMatrix.initialize()
+        tnexMatrix.initialize("https://chat-matrix.tnex.com.vn", "https://graviteeioapigw-preprod.tnex.vn/api/v1/customer-gw/services/uploadFiles")
+        val token = "Bearer eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyNjMwY2I5Yy01NzcxLTRkZWUtOTVjZC1hZDkyYWNjNTg4ZGMiLC" +
+                "JmaXJzdG5hbWUiOiJOR1VZ4buETiBUSOG7iiBCScOKTiIsInJvbGUiOlsiVDEiXSwiaXNzIjoiaHR0cDovL2dyYXZp" +
+                "dGVlaW9hbWd5LXVhdC50bmRtYXJrZXRwbGFjZS5jb20vbXNiL29pZGMiLCJtb2JpbGUiOiIwOTY4OTY1NjU4IiwicG" +
+                "VybWlzc2lvbiI6ImZ1bGwtYWNjZXNzIiwiZGV2aWNlLWlkIjoiNjc2NjczZDAxOTIxMzA2YiIsImxhc3RuYW1lIjoi" +
+                "IiwiYXVkIjoibXNiX2NsaWVudCIsImN1c3RvbWVyLWlkIjoiY2Q4N2I1YjEtNjgzYS00ZmU4LThmOWQtNGRiZGJlZW" +
+                "Y2NTBiIiwiY2lmLW51bWJlciI6IjE2MzI0NzU4MDMxMzY0IiwiZG9tYWluIjoibXNiIiwibWVyY2hhbnQtaWQiOiIi" +
+                "LCJleHAiOjE2NDg2OTY4NjksImlhdCI6MTY0ODY5NTk2OSwianRpIjoiMjIwMzMxMTAwNjA5MDk2MDI5In0.NX8Q5I" +
+                "nQ8fdxgCmTStPT08UA6h3NdcnbW0qkO_MqT3IDRV8y2C22qPgxfPNrZ0tcvQZ_O1hWx0BaO1qOXu8Vpw4QqY6u7Tov" +
+                "naNtFBOjMYvJM_Go2IZthO4b4FYBRYZXl5v-Zk9Zb6vK6yKtUqCHRFmAqJKjnk2zkjegXXn9liaGe8_-BUr_qz1Qy9" +
+                "Iz6idi2uGYdr2moWSP6th6gKJ0ioWTFdvHwgagG03XoK0cDdeMVBeozzO-o9GwuiI_ljBr38wOKULlrpqAi-WAPwEh" +
+                "RBfWi79UHi48_u3rI-MMiFZ2skiYJjwdjz56YHNtQs-aCrOJwBn15naQxr6aggLAUg"
+        val deviceId = "676673d01921306b"
+        val langguage = "vi"
+        val local = "21.0233792,105.8096129"
+        tnexMatrix.updateUserUploadInfo(deviceId, token, local, langguage);
     }
 
     private fun initAppData() {
