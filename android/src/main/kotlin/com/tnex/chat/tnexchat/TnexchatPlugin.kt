be7fe9afcedd7b50659c6f14c968fd8dde50cc31
hoangson8011 <sonhs1@tnex.com.vn>
2022-04-04 17:41:54 +0700

    change root project

--- android/src/main/kotlin/com/tnex/chat/tnexchat/TnexchatPlugin.kt
+++ android/src/main/kotlin/com/tnex/chat/tnexchat/TnexchatPlugin.kt
@@ -1,21 +1,24 @@
 package com.tnex.chat.tnexchat
 
+import android.app.Activity
 import androidx.annotation.NonNull
-import com.tnex.matrix.AppTestInfo
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** TnexchatPlugin */
-class TnexchatPlugin: FlutterPlugin, MethodCallHandler {
+class TnexchatPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var activity : Activity
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "tnexchat")
@@ -24,7 +27,11 @@ class TnexchatPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
-      result.success("Android ${AppTestInfo.getAndroidVersion()}")
+      val tokenFake = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5NGVkM2IwNi1lN2E1LTQ2ODMtOWM0Ni0wZjc2YzEyZDlhYTAifQ.43m-TAjkt8AqHv1JGeAIJMrU-k2kdn-qh1p6FLCcd-Y"
+
+      val roomId = "!YKsBgFhGFwNhpbTxdZ:chat-matrix.tnex.com.vn"
+      MatrixApplication.sInstance.tnexMatrix.showChatDetail(roomId, tokenFake);
+      //result.success("Android ${AppTestInfo.getAndroidVersion()}")
     } else {
       result.notImplemented()
     }
@@ -33,4 +40,22 @@ class TnexchatPlugin: FlutterPlugin, MethodCallHandler {
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    //TODO("Not yet implemented")
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    //TODO("Not yet implemented")
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    //TODO("Not yet implemented")
+  }
+
+  override fun onDetachedFromActivity() {
+    //TODO("Not yet implemented")
+  }
+
 }
--- tnexchat/example/android/app/src/main/kotlin/com/tnex/chat/tnexchat_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tnex.chat.tnexchat_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
